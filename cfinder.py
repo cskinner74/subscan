8cc663a7cd1a111dbbe6e3bf7a08ca00559b3004
root <cody@codyskinner.net>
2019-09-26 03:36:43 -0400

    added cfinder tool

---
+++ cfinder.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+#cfinder.py
+#Developer: Cody Skinner
+#ver 0.1.1
+
+import subprocess
+import sys
+import getopt
+import re
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, 'hi:o:',['ifile=','ofile='])
+    except getopt.GetoptError:
+        print('usage: cfinder.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('cfinder.py')
+            print('Tool for finding hanging CNAME entries')
+            print('usage: cfinder.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ('-i', '--ifile'):
+            inputfile = arg
+        elif opt in ('-o','--ofile'):
+            outputfile = arg
+    if not inputfile or not outputfile:
+        print('usage: cfinder.py -i <inputfile> -o <outputfile>')
+        sys.exit()
+    with open(inputfile, 'r') as i:
+        line = i.readline().rstrip()
+        with open(outputfile, 'a') as out:
+            while line:
+                print('Checking ',str(line))
+                cmd = ['dig', line]
+                pull = ['grep','CNAME']
+                p1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True)
+                p2 = subprocess.Popen(pull, stdin=p1.stdout, stdout=subprocess.PIPE, text=True)
+                p1.stdout.close()
+                result = p2.communicate()[0]
+                if result != '':
+                    print(str(result))
+                    out.write(str(result))
+                line = i.readline().rstrip()
+    print('Output saved to ', outputfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
